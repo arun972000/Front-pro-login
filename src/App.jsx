6d78fe3b0bbe703e6263dfb9837e2847c2b8eb18
arun972000 <128138897+arun972000@users.noreply.github.com>
2023-09-15 12:29:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+import Login from './Components/login'
+import ResetPasswordForm from './Components/Password Reset/resetPassForm'
+import Verify from './Components/Password Reset/verify'
+import ResetPass from './Components/Password Reset/resetPass'
+import { Routes, Route } from "react-router-dom"
+import RegistrationForm from './Components/Register'
+
+function App() {
+
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Login />}/>
+        <Route path="/register" element={<RegistrationForm/>}/>
+        <Route path="/passreset" element={<ResetPass/>}/>
+        <Route path="/verifypass" element={<Verify/>}/>
+        <Route path="passResetForm" element={<ResetPasswordForm/>}/>
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Password Reset/verify.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+const Verify = () => {
+  const [params] = useSearchParams();
+  const [verified, setVerified] = useState(false);
+  const navigate=useNavigate()
+
+  useEffect(() => {
+    const resetToken = localStorage.getItem("resetToken");
+    const paramsToken = params.get('token');
+    
+    if (resetToken === paramsToken) {
+      setVerified(true);
+    }
+  }, [params]);
+
+  return (
+    <div>
+      {verified ? navigate("/passResetForm") : "not verified"}
+    </div>
+  );
+}
+
+export default Verify;
