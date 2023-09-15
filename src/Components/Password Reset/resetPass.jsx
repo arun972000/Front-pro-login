87dcfa7e0052828a8be95d8047460694c4ec16f6
arun972000 <128138897+arun972000@users.noreply.github.com>
2023-09-15 14:52:48 +0530

    changed many

--- src/Components/Password Reset/resetPass.jsx
+++ src/Components/Password Reset/resetPass.jsx
@@ -1,54 +1,44 @@
 import axios from "axios"
 import { Url } from "../Url"
 import { useNavigate } from "react-router-dom"
-// import Verify from "./verify"
-
 import { useState } from "react"
 
+
 const ResetPass = () => {
-  const [email, setemail] = useState("")
-  const navigate=useNavigate()
+  const [email, setemail] = useState('');
+ // State to hold the verify_link
+  const navigate = useNavigate();
 
   const passreset = async () => {
-    try{
-      const res = await axios.post(`${Url}api/resetPass`, { email })
-      const {token}=res.data;
-      console.log(token)
-      alert("please check your mail inbox to verify")
-      navigate("/")
-    }catch(err){
-      alert("enter valid email")
+    try {
+     const res=await axios.post(`${Url}api/resetPass`, { email });
+     localStorage.setItem("token",res.data)
+      alert('Please check your mail inbox to verify');
+      navigate('/');
+    } catch (err) {
+      alert('Enter a valid email');
     }
-
-  }
-
-
+  };
   return (
-    
-      <div className="container mt-5 pt-5">
-        <div className="row">
-          <div className="col-12 col-sm-8 col-md-6 m-auto">
-            <div className="card">
-              <div className="card-body">
-                <div className="mb-3">
-                  <label htmlFor="email" className="form-label">Email</label>
-                  <input type="email" className="form-control mb-3" name="email" value={email} placeholder="Enter Registered Email" onChange={(e) => setemail(e.target.value)}
-                    id="email" />
-                    <div className="text-center">
-                    <button className="btn btn-primary" onClick={passreset}>submit</button>
-                    {/* {token && <Verify token={token}/>} */}
-                    </div>
-                  
+    <div className="container mt-5 pt-5">
+      <div className="row">
+        <div className="col-12 col-sm-8 col-md-6 m-auto">
+          <div className="card">
+            <div className="card-body">
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label">Email</label>
+                <input type="email" className="form-control mb-3" name="email" value={email} placeholder="Enter Registered Email" onChange={(e) => setemail(e.target.value)} id="email" />
+                <div className="text-center">
+                  <button className="btn btn-primary" onClick={passreset}>submit</button>
                 </div>
               </div>
             </div>
           </div>
         </div>
-
       </div>
-   
-  )
+    </div>
+  );
 }
 
-export default ResetPass
+export default ResetPass;
 
--- src/Components/Password Reset/verify.jsx
+++ src/Components/Password Reset/verify.jsx
@@ -2,12 +2,13 @@ import { useState, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
+
 const Verify = () => {
   const [params] = useSearchParams();
   const [verified, setVerified] = useState(false);
   const navigate=useNavigate()
 
- 
+
 
   useEffect(() => {
     const resetToken = localStorage.getItem("resetToken");
