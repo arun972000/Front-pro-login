32200790d8ef463132e5402cb95a86ce3cfe763b
arun972000 <128138897+arun972000@users.noreply.github.com>
2023-09-20 18:46:35 +0530

    changed back

--- src/Components/Password Reset/verify.jsx
+++ src/Components/Password Reset/verify.jsx
@@ -1,41 +1,69 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useState } from "react";
 import { Url } from "../Url";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-const PasswordVerify = () => {
-  
-  const [params,]=useSearchParams()
+const Verify = () => {
+  const [token, setToken] = useState('');
 
- 
+  const [verificationStatus, setVerificationStatus] = useState('');
 
   const navigate = useNavigate()
 
-  
+  const handleTokenChange = (e) => {
+    setToken(e.target.value);
+  };
 
   const verifyingApi = async () => {
     try {
-      await axios.post(`${Url}api/tokenVerify`, { token:params.get("token") })
-      localStorage.setItem("verifyToken",params.get("token"))
-      navigate("/passwordForm")
+      await axios.post(`${Url}api/tokenVerify`, { token })
+      localStorage.setItem("verifyToken",token)
+      navigate("/passResetForm")
     } catch (err) {
       console.log(err)
-     
+      setVerificationStatus('Token is invalid.');
     }
 
 
   }
 
-useEffect(()=>{
-  verifyingApi()
-// eslint-disable-next-line react-hooks/exhaustive-deps
-},[])
-  return (
-<>
-</>
+  
+  const handleVerifyToken = () => {
+    verifyingApi()
 
+  }
+  return (
+
+
+    <div className="container mt-5 pt-5">
+      <div className="row">
+        <div className="col-12 col-sm-8 col-md-6 m-auto">
+          <div className="card"><div className="card-body">
+            <h1>Token Verification</h1>
+            <div className="mb-3">
+              <label htmlFor="tokenInput" className="form-label">
+                Enter Token:
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="tokenInput"
+                value={token}
+                onChange={handleTokenChange}
+              />
+            </div>
+            <button className="btn btn-primary" onClick={handleVerifyToken}>
+              Verify Token
+            </button>
+            <div className="mt-3">
+              <p>{verificationStatus}</p>
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
-export default PasswordVerify;
-
+export default Verify;
